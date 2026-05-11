539a5a8dd75fa382e2a957427381ee0ade12560a
FalconFlash20 <fjlf2004@gmail.com>
2026-05-11 09:32:09 +0200

    update: ClienteService

--- P1/src/main/java/com/mgcss/service/ClienteService.java
+++ P1/src/main/java/com/mgcss/service/ClienteService.java
@@ -1,5 +1,24 @@
 package com.mgcss.service;
 
-public class ClienteService {
+import java.util.List;
 
+import com.mgcss.domain.Cliente;
+import com.mgcss.domain.ClienteRepository;
+
+public class ClienteService {
+	private final ClienteRepository clienterepository;
+
+	public ClienteService(ClienteRepository cliente) {
+		this.clienterepository = cliente;
+	}
+	public void crearCliente(Cliente c) {
+		clienterepository.save(c);
+	}
+	public Cliente consultarTecnico(Long ClienteId) {
+    	return clienterepository.findById(ClienteId).orElseThrow(() -> new IllegalArgumentException("No se ha podido consultar el cliente"));		
+	}
+	public List<Cliente> listarTecnicos() {
+		return clienterepository.findAll();
+	}
+	
 }
--- P1/src/main/java/com/mgcss/service/TecnicoService.java
+++ P1/src/main/java/com/mgcss/service/TecnicoService.java
@@ -1,5 +1,34 @@
 package com.mgcss.service;
 
-public class TecnicoService {
+import java.util.List;
 
+import com.mgcss.domain.Tecnico;
+import com.mgcss.domain.TecnicoRepository;
+
+public class TecnicoService {
+	private final TecnicoRepository tecnicoRepository;
+
+	public TecnicoService(TecnicoRepository tecnicoRepository) {
+		this.tecnicoRepository = tecnicoRepository;
+	}
+	public void crearTecnico(Tecnico t) {
+		tecnicoRepository.save(t);
+	}
+	public Tecnico consultarTecnico(Long TecnicoId) {
+    	return tecnicoRepository.findById(TecnicoId).orElseThrow(() -> new IllegalArgumentException("No se ha podido consultar el tecnico"));		
+	}
+	public List<Tecnico> listarTecnicos() {
+		return tecnicoRepository.findAll();
+	}
+	public void activar(Long TecnicoId) {
+		Tecnico t=consultarTecnico(TecnicoId);
+		t.activar();
+		tecnicoRepository.save(t);
+	}
+	public void desactivar(Long TecnicoId) {
+		Tecnico t=consultarTecnico(TecnicoId);
+		t.desactivar();
+		tecnicoRepository.save(t);
+	}
+	
 }
