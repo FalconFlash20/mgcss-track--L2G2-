4430357e6657d34dcfbfc3ef14025643b1fd35bc
FalconFlash20 <fjlf2004@gmail.com>
2026-05-11 09:12:31 +0200

    update: SolicitudService && TecnicoRepository

--- P1/src/main/java/com/mgcss/domain/TecnicoRepository.java
+++ P1/src/main/java/com/mgcss/domain/TecnicoRepository.java
@@ -1,9 +1,11 @@
 package com.mgcss.domain;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TecnicoRepository {
-	
+	Tecnico save(Tecnico tecnico);
 	Optional<Tecnico> findById(Long id);
+	List<Tecnico> findAll();
 }
 
--- P1/src/main/java/com/mgcss/service/SolicitudService.java
+++ P1/src/main/java/com/mgcss/service/SolicitudService.java
@@ -1,6 +1,8 @@
 package com.mgcss.service;
 
+import java.util.List;
 import com.mgcss.domain.Solicitud;
+import com.mgcss.domain.Solicitud.EstadoSolicitud;
 import com.mgcss.domain.SolicitudRepository;
 import com.mgcss.domain.Tecnico;
 import com.mgcss.domain.TecnicoRepository;
@@ -25,5 +27,28 @@ public class SolicitudService {
         solicitud.asignarTecnico(tecnico);
 
         solicitudRepository.save(solicitud);
+    }
+    public void crearSolicitud(Solicitud s) {
+    	solicitudRepository.save(s);
+    }
+    public Solicitud consultarSolicitud(Long SolicitudId) {
+    	return solicitudRepository.findById(SolicitudId).orElseThrow(() -> new IllegalArgumentException("No se ha podido reabrir la solicitud"));
+    }
+    public void cambiar(Long SolicitudId, EstadoSolicitud estado) {
+    	Solicitud s=consultarSolicitud(SolicitudId);
+    	if(estado==EstadoSolicitud.CERRADA) {
+    		s.cerrar();
+    	} else if(estado==EstadoSolicitud.EN_PROCESO) {
+    		s.iniciarProceso();
+    	}
+    	solicitudRepository.save(s);
+    }
+    public void reabrirSolicitud(Long SolicitudId) {
+    	Solicitud s=consultarSolicitud(SolicitudId);
+    	s.reabrir();
+    	solicitudRepository.save(s);
+    }
+    public List<Solicitud> listar() {
+    	return solicitudRepository.findAll();
     }
 }
